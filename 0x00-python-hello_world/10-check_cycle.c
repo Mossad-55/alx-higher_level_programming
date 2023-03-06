3ac175e4a34c58828184a64cc8c0e9112b89bdf7
Mossad-55 <mosad55522@gmail.com>
2023-03-06 23:58:11 +0200

    Task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * check_cycle - finds if there any cycle in a linkedlist.
+ * @list: pointer to the linked list.
+ * Return: int (0 Or 1).
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list.next;
+
+	while (fast != null && slow != fast)
+	{
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+
+	return (fast == slow);
+}
