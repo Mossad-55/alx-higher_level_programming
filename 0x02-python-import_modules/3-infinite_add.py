80f2cc4d155c0eff37cb7998d6c8cbe77285ee40
Mossad-55 <mosad55522@gmail.com>
2023-03-12 22:22:21 +0200

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    argv = sys.argv
-    argc = len(argv) - 1
-    if argc == 0:
-        print("{:d}".format(0))
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
         return
     else:
         i = 1
         res = 0
-        while i <= argc:
+        while i <= n:
             res += int(argv[i])
             i += 1
         print("{:d}".format(res))
+
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
