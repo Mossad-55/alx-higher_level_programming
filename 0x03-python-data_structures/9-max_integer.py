977bd283d9e532c94b449b6620c67085a9cdc2b7
Mossad-55 <mosad55522@gmail.com>
2023-03-14 22:19:55 +0200

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_num = 0
+    for n in my_list:
+        if n > max_num:
+            max_num = n
+    return max_num
