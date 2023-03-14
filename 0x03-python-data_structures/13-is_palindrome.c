64f92a77ce9b8bf21e88a6423dcd00ae4050e43f
Mossad-55 <mosad55522@gmail.com>
2023-03-14 22:54:19 +0200

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,41 +1,36 @@
 #include "lists.h"
-
 /**
- * is_palindrome - checks if a list is palindrome or not.
+ * is_palindrome - detects palindromic singly linked lists
  * @head: head of the list
  *
- * Return: 1 if palindrome, 0 if not.
+ * Return: 1 if palindrome, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *cmcpy;
+	listint_t *cmpcpy;
 	listint_t *cpy;
 	unsigned int len, i;
 
 	if (head == NULL)
 		return (1);
-
 	cpy = *head;
-	cmcpy = *head;
-	for (len = 0; cmcpy->next != NULL; len++)
-		cmcpy = cmcpy->next;
-
+	cmpcpy = *head;
+	for (len = 0; cmpcpy->next != NULL; len++)
+		cmpcpy = cmpcpy->next;
 	while (len > 0)
 	{
-		if (cpy->n == cmcpy->n)
+		if (cpy->n == cmpcpy->n)
 		{
 			len--;
 			cpy = cpy->next;
-			cmcpy = *head;
+			cmpcpy = *head;
 			for (i = len; i > 0; i--)
-				cmcpy = cmcpy->next;
+				cmpcpy = cmpcpy->next;
 		}
 		else
 			break;
 	}
-
-	if (cpy->n == cmcpy->n)
+	if (cpy->n == cmpcpy->n)
 		return (1);
-
 	return (0);
 }
